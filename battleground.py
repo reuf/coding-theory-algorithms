849d9affc1ccf39610d308dbe979e86b8bfec585
christiansiegel <>
2016-03-31 07:38:32 +0200

    battleground for exam

---
+++ battleground.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+
+from BinarySymmetricChannel2 import BSC
+from GaloisField import GaloisField
+from GaloisField import GF2
+from LinearBlockCode import LinearBlockCode
+from CyclicCode import CyclicCode
+from BCHCode import BCHCode
+from RSCode import RSCode
+
+import GaloisField as gField
+import LinearBlockCode as lCode
+import CyclicCode as cCode
+import BCHCode as bCode
+
+""" There Is Beer at the End of the Tunnel! """
+
+if __name__ == '__main__':
+    pass
